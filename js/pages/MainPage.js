0c01fb4100fd7091bd61b643e2766dcee6384e57
ofir mossinson <ofirmoss@gmail.com>
2017-12-06 16:59:37 +0200

    mainPage+nevigationMix

--- js/pages/MainPage.js
+++ js/pages/MainPage.js
@@ -1,10 +1,23 @@
+import pagesNavigationMixin from '../mixins/pagesNavigationMixin.js'
+
 
 export default {
     template: `
-        <section >
-           <div class="mailIcon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
-           <div class="noteIcon"><i class="fa fa-sticky-note-o" aria-hidden="true"></i></div>
-           <div class="mapIcon"><i class="fa fa-globe" aria-hidden="true"></i></div>           
+        <section class="homepageContainer">
+     
+        <div @click="navigateToMail" class="iconContainer mailIcon">
+                <div class="mailIcon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+        </div>
+        
+        <div @click="navigateToNotes" class="iconContainer noteIcon">
+            <div class="noteIcon"><i class="fa fa-sticky-note-o" aria-hidden="true"></i></div>        
+        </div>
+
+        
+        <div @click="navigateToMap" class="iconContainer mapIcon">
+            <div class="mapIcon"><i class="fa fa-globe" aria-hidden="true"></i></div>           
+        </div>
+
         </section>
     `,
     data() {
@@ -22,5 +35,6 @@ export default {
     },
     created() {
         console.log('Created Main Page Comp');
-    }
+    },
+     mixins: [pagesNavigationMixin]
 }
