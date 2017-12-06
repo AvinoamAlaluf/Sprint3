fb09a3824f6a925034afdb77deb68efffe2e473d
ofir mossinson <ofirmoss@gmail.com>
2017-12-06 17:11:58 +0200

    mergeing

---
+++ js/comps/mailComps/mailsList.js
@@ -0,0 +1,23 @@
+export default {
+    template: `
+        <section >
+        
+        <table>
+        <tr v-for="(mail, idx) in mails"></tr>
+
+        </table>
+
+        </section>
+    `,
+    data() {
+        return {
+         
+        }
+    },
+    methods: {
+      
+    },
+    created() {
+        
+    }
+}
