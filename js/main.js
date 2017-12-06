89dceb25038542d0eebc9af2d26fa156bfc75226
Avinoam Alaluf <avinoamalaluf@gmail.com>
2017-12-06 12:48:20 +0200

    first Template Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import routes from 'routes/js';
+
+Vue.use(VueRouter);
+const myRouter = new VueRouter({ routes: myRouter });
+
+var myVue = new Vue({
+    template: `
+        <section>
+            <h1>Sprint 3 Bitch Madafacuka</h1>    
+        </section>    
+    `,
+    created() {
+
+    },
+    router: myRouter
+
+}).mount('#app')
