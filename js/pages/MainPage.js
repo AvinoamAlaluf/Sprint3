f08791b347bbf8d76d95ad52080cedcd81e48977
Avinoam Alaluf <avinoamalaluf@gmail.com>
2017-12-06 17:05:32 +0200

    change comments on main.js

--- js/pages/MainPage.js
+++ js/pages/MainPage.js
@@ -3,16 +3,15 @@ import pagesNavigationMixin from '../mixins/pagesNavigationMixin.js'
 
 export default {
     template: `
-<<<<<<< HEAD
+
         <section >
-<<<<<<< HEAD
+
            
-=======
+
            <div class="mailIcon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
            <div class="noteIcon"><i class="fa fa-sticky-note-o" aria-hidden="true"></i></div>
            <div class="mapIcon"><i class="fa fa-globe" aria-hidden="true"></i></div>           
->>>>>>> 4b753a6906378e32ae9e71290b44a0458c3aaf29
-=======
+
         <section class="homepageContainer">
      
         <div @click="navigateToMail" class="iconContainer mailIcon">
@@ -28,7 +27,7 @@ export default {
             <div class="mapIcon"><i class="fa fa-globe" aria-hidden="true"></i></div>           
         </div>
 
->>>>>>> 0c01fb4100fd7091bd61b643e2766dcee6384e57
+
         </section>
     `,
     data() {
