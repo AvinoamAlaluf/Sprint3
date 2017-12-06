cd79f5f9844ee187ef4723d3aaa7d5b323becd2a
ofir mossinson <ofirmoss@gmail.com>
2017-12-06 20:24:13 +0200

    dsa

--- js/pages/MailPage.js
+++ js/pages/MailPage.js
@@ -5,16 +5,9 @@ import MailMenu from '../comps/mailComps/MailMenu.js'
 export default {
     template: `
         <section class="emailContainer">
-<<<<<<< HEAD
-            <mail-menu></mail-menu>
-            <div class="emailRightSection">
-            <mails-list></mails-list>
-            </div>
-=======
         <h1>mail</h1>
             <mail-menu  @getInbox="sortByDate()"></mail-menu>
             <mails-list :emails="emails" ></mails-list>
->>>>>>> 34dd7906387df7a25b4859ed3f6e1c28085286c9
         </section>
     `,
     data() {
